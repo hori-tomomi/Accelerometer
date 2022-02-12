a9d5fb01cabae9ef83945268947ce491483d2429
TomomiHori <hr.tmm.23@icloud.com>
2022-02-13 02:25:55 +0900

    Initial Commit

---
+++ Accelerometer/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Accelerometer
+//
+//  Created by Tomomi Hori on 2022/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToViewController(segue: UIStoryboardSegue){
+        
+    }
+
+
+}
+
